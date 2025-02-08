a1a689425445688bd53c8eccba79c0820d38da25
MisbahNayab12 <160389499+MisbahNayab12@users.noreply.github.com>
2025-02-08 12:56:34 +0500

    updated

--- app/Products/[id]/page.tsx
+++ app/Products/[id]/page.tsx
@@ -1,18 +1,11 @@
 import { client } from "@/sanity/lib/client";
-import ProductDetail from "../../component/ProductDetail"; // ✅ Create this Client Component
+import ProductDetail from "../../component/ProductDetail"; 
 
 interface ProductPageProps {
   params: { id: string };
 }
 
-export async function generateStaticParams() {
-  const products = await client.fetch(`*[_type == "food"]{ _id }`);
-  return products.map((product: { _id: string }) => ({ id: product._id }));
-}
-
 export default async function ProductPage({ params }: ProductPageProps) {
-
-  console.log("Params received in ProductPage:", params)
 
   const product = await getProduct(params.id);
 
